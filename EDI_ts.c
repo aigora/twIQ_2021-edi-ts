583b1ea264ba663a59670ab5cf5a23f0a53699f2
e-aliaga <80335767+e-aliaga@users.noreply.github.com>
2021-04-17 18:18:56 +0200

    Update EDI_ts.c

--- EDI_ts.c
+++ EDI_ts.c
@@ -20,12 +20,36 @@ int main (){
         }
     
     if (idioma=='a'){
-        printf("�Qu� tienes en la nevera?\n");
-        printf("Por categor�as\n");
+        printf("¿Qué tienes en la nevera?\n");
+        printf("Por categorías\n");
         printf("Ver todas las recetas\n");
         printf("Festividades\n");
-        printf("Crea tu men�\n");
+        printf("Crea tu menú\n");
         printf("SALIR\n");
+        printf("Marque el numero de la opcion deseada:\n");
+        scanf("%d",&opcion);
+        if(opcion==4){
+        printf(" 1.Navidad.\n 2.San Valentin.\n 3.Cumpleannyos.\n 4.Semana Santa.\n");
+        printf("Introduzca el numero de la festividad deseada:\n");
+        scanf("%d",&o1);
+                if (o1==1){
+                    printf("*¡¡Feliz Navidad!!*\n Mas tiempo con tu familia, menos pensando el menu\n\n");
+                    printf(" 1.Menu estandar\n 2.Menu premium\n 3.Menu kids\n");
+                    printf("Introduzca el numero del menu deseado:\n");
+                    scanf("%d",&o2);
+                    if (o2==1){
+                        printf("Menu estandar:\n\n");
+                        printf(" *ENTRANTES:*\n Embutido Variado\n Humus de garbanzo. Acompañado de nachos, zanahoria, o puerros\n Croquetas de jamon\n\n *PRIMER PLATO:*\n Crema de calabaza con queso roquefort\n Sopa de marisco\n Tempura de verduras\n\n *SEGUNDO PLATO:*\n Solomillo Wellington\n Berenjenas rellenas\n Lubina a la sal\n\n *POSTRES:*\n Tarta de queso/zanahoria\n Brownie\n Hojaldre de chocolate\n");
+                    }else if (o2==2){
+                        printf("Menu premium:\n\n");
+                        printf(" *ENTRANTES:*\n Sticktartar\n Coquinas en salsa marinera\n Jamon 5jotas\n Salmon gravelax\n\n *PRIMER PLATO:*\n Risoto con trufas\n Bisque de langostinos\n Crema de carabineros\n\n *SEGUNDO PLATO:*\n Cordero asado a las finas hierbas\n Merluza en salsa verde\n Lasaña de verduras\n\n *POSTRES:*\n Mousse de crema de orujo\n Trufas con helado flambeado\n Filloas\n\n");
+                    }else if(o2==3){
+                        printf("Menu kids:\n\n");
+                        printf(" *PLATO PRINCIPAL:*\n Macarrones gratinados\n Corquetas y empanadillas\n Escalopines con patatas\n Arroz a la cubana\n\n *POSTRES:*\n Bola de helado\n Flan de queso\n Mousse de chocolate\n Macedonia\n");
+                        
+                    }
+                }
+        }
     }else if (idioma=='b') {
         printf("What do you have in your fridge?\n");
         printf("Categories\n");
